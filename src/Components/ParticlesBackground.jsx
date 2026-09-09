5d0ed64123f8cecf76760334544d741f774f424c
sebastian <sebastian.navarropiedra@ucreativa.com>
2026-09-08 19:53:40 -0600

    .

--- src/Components/ParticlesBackground.jsx
+++ src/Components/ParticlesBackground.jsx
@@ -3,9 +3,9 @@ import '../Styles/Particles.css'; // your CSS file
 
 function ParticlesBackground() {
   return (
-    <div className="particlesContainer alignfull">
+    <div className="particlesContainer ">
       <div className="bubbles">
-        {Array.from({ length: 20 }).map((_, i) => (
+        {Array.from({ length: 15 }).map((_, i) => (
           <span key={i} style={{ '--i': Math.floor(Math.random() * 18) + 3 }}></span>
         ))}
       </div>
